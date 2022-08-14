489ce2889841418b7ae00afb130e20b1f0ab7f35
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2022-08-13 21:00:44 -0300

    styling spp

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -10,7 +11,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
+        <Avatar
           src="https://github.com/matheuseufrasio2.png"
         />
         <strong>Matheus Eufrásio</strong>
